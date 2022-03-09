4c2798a65ed5baedc4cbc3e55739242d6813e725
Janosch <mail@janjo.ch>
2022-03-09 13:33:43 +0000

    flake8 compatibility

--- sum_nested.py
+++ sum_nested.py
@@ -1,19 +1,30 @@
+"""Add up all values in dicts, lists or tuples.
+
+Nested structures are added up recursively.
+"""
+
+
 def sum_nested(inp):
-    """Adds up all values in dicts, lists or tuples. Works recursively.
+    """Add up all values in dicts, lists or tuples.
+
+    Nested structures are added up recursively.
     """
-    
     if(type(inp) is dict):
         inp = [elem for key, elem in inp.items()]
-    
+
     if(type(inp) is list or type(inp) is tuple):
         val = 0
         for elem in inp:
-            if(type(elem) is dict or type(elem) is list or type(elem) is tuple):
+            if(
+                type(elem) is dict
+                or type(elem) is list
+                or type(elem) is tuple
+            ):
                 val += sum_nested(elem)
-            
+
             else:
                 val += elem
-        
+
         return(val)
-    
+
     return(inp)
