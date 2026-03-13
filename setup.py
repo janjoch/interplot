b0c3af7ed6edad781e09165288366f2a51176d0c
Janjo <janjo@duck.com>
2026-03-13 16:52:15 +0100

    bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="interplot",
-    version="1.1.2",
+    version="1.2.0",
     description=(
         "Create matplotlib and plotly charts with the same few lines of code."
     ),
